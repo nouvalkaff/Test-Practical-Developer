4bf8e0eff91191f0ec005a8e2a54b8f8773b2195
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-28 02:34:00 +0700

    add folder structure and db config

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+Buat design arsitektur dan prosesnya berupa flow gambar dari konsep microservices di dalam docker container yang terdiri dari database dan aplikasi.
+
+Kasus: Kita akan deploy beberapa aplikasi terdiri dari frontend & backend (API), di dalam backend terdiri 2 service yaitu service users & Katalog.
+
+Semua services di protect dengan token JWT untuk mengambil datanya.
+
+Notes: Jangan lupa memberikan keterangan dari masing2 service dan databasenya.
+
+Contoh DB = MongoDB/PostgreSql/MySql,Lang Program = nodejs/php/java/python
+*/
